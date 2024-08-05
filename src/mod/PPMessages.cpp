2e2d3fba8a9017653802ecbb9a46df84f5328845
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-05 18:39:24 +0300

    Create PPMessages.cpp

---
+++ src/mod/PPMessages.cpp
@@ -0,0 +1,28 @@
+#include "PPMessages.h"
+
+YAML::Node PPMessages::getNestedNode(const std::string& node) 
+{
+        YAML::Node currentNode = messages;
+        std::stringstream ss(node);
+        std::string segment;
+
+        while (std::getline(ss, segment, '.')) {
+            if (currentNode[segment]) {
+                currentNode = currentNode[segment];
+            } else {
+                return YAML::Node();
+            }
+        }
+
+        return currentNode;
+}
+
+std::string PPMessages::replaceString(std::string str, const std::string& from, const std::string& to) 
+{
+        size_t startPos = 0;
+        while ((startPos = str.find(from, startPos)) != std::string::npos) {
+            str.replace(startPos, from.length(), to);
+            startPos += to.length();
+        }
+        return str;
+}
