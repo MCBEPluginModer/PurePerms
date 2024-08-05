97c86d788ad4ecba35a67d958ee4dc9ef215d00f
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-05 18:34:16 +0300

    Update PPMessages.h

--- src/mod/PPMessages.h
+++ src/mod/PPMessages.h
@@ -9,7 +9,14 @@ class PPMessages
 {
    vector<string> langList;
    YAML::Node messages;
-   std::string language;
+   string language;
+   string replaceString(string str, const string& from, const string& to);
+   YAML::Node getNestedNode(const string& node);
 public:
    PPMessage();
+   void registerLanguages();
+   std::string getMessage(const string& node, const vector<string>& vars = {});
+   inline string getVersion() {return messages["messages-version"].as<string>();}
+   void loadMessages();
+   void reloadMessages();
 };
