290e89cf9eba2fdca07956965ff4e3e87c1348b4
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-06 19:33:32 +0300

    Update PPGroup.h

--- src/mod/PPGroup.h
+++ src/mod/PPGroup.h
@@ -7,6 +7,7 @@
 #include <mc/world/actor/player/Player.h>
 #include "MyMod.h"
 #include <variant>
+#include <tuple>
 using namespace std;
 
 class PPGroup
@@ -26,4 +27,13 @@ public:
    PPGroup* getNode(string node);
    vector<PPGroup*> getParentGroups();
    YAML::Node getWorldData(string levelName);
+   YAML::Node getWorldNode(string levelName,string node);
+   bool isDefault(string levelName = "");
+   void removeNode(string node);
+   bool removeParent(PPGroup* parent);
+   void removeWorldNode(string levelName,string node);
+   void setData(tuple<string,bool,vector<string>,vector<string>,YAML::Node> data);
+   void setDefault(string levelName = "");
+   bool setGroupPermission(string permission,string levelName = "");
+   void setNode(string node,variant<int,float,double,string> value);
 };
