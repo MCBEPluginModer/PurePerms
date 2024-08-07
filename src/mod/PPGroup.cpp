e3acde30d76075139b7dfe6435089d4431f6bb61
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-07 19:01:21 +0300

    Update PPGroup.cpp

--- src/mod/PPGroup.cpp
+++ src/mod/PPGroup.cpp
@@ -19,13 +19,20 @@ bool PPGroup::addParent(PPGroup* parent)
     get<0>(data)["inheritance"].push_back(parent->getName());
     tuple<string,bool,vector<string>,vector<string>,YAML::Node> data1 = std::make_tuple<string,bool,vector<string>,vector<string>,YAML::Node>(get<0>(data)["alias"].as<string>(),get<0>(data)["isDefault"].as<bool>(),get<0>(data)["inheritance"].as<vector<string>>(),get<0>(data)["permissions"].as<vector<string>>(),get<0>(data)["worlds"].as<YAML::Node>());
     setData(data1);
-    /*data = getData(); // Retrieve the current data
-    data->push_back(group.getName()); // Add the group's name to the inheritance
-
-    setData(data); // Update the data*/
-   // tuple<string,bool,vector<string>,vector<string>,YAML::Node> data;
-    
     //plugin->updatePlayersInGroup(*this); // Notify the plugin to update players in this group
 
     return true;
+}
+
+void PPGroup::createWorldData(string levelName)
+{
+    auto data = getData();
+    YAML::Node& worlds = std::get<0>(data)["worlds"];
+        
+    if (!worlds[levelName]) 
+    {
+        worlds[levelName]["isDefault"] = false;
+        worlds[levelName]["permissions"] = YAML::Node(YAML::NodeType::Sequence);
+        setData(data);
+    }
 }
