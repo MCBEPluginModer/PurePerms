ca8ca59ae16f9c4aa6cdd119da4c2037ecdbd90d
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-07 17:57:24 +0300

    Update PPGroup.cpp

--- src/mod/PPGroup.cpp
+++ src/mod/PPGroup.cpp
@@ -5,12 +5,16 @@ void PPGroup::addParent(PPGroup* parent)
     if (this == &group || std::find(parentGroups.begin(), parentGroups.end(), group.getName()) != parentGroups.end()) {
         return false;
     }
-
-    data = getData(); // Retrieve the current data
+    data = getData(); 
+    get<YAML::Node>(data)["inheritance"].push_back(parent->getName());
+    tuple<string,bool,vector<string>,vector<string>,YAML::Node> data1(getName(), get<YAML::Node>(data)["alias"].as<string>(),get<YAML::Node>(data)["isDefault"].as<bool>(),get<YAML::Node>(data)["inheritance"].as<vector<string>>(),get<YAML::Node>(data)["permissions"].as<vector<string>>(),get<YAML::Node>(data)["worlds"]);
+    setData(data1);
+    /*data = getData(); // Retrieve the current data
     data->push_back(group.getName()); // Add the group's name to the inheritance
 
-    setData(data); // Update the data
-
+    setData(data); // Update the data*/
+    tuple<string,bool,vector<string>,vector<string>,YAML::Node> data;
+    
     //plugin->updatePlayersInGroup(*this); // Notify the plugin to update players in this group
 
     return true;
