75934ff4420139d056746b3a51e8dcea43f74614
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-10 19:22:06 +0300

    Update DefaultProvider.h

--- src/mod/provider/DefaultProvider.h
+++ src/mod/provider/DefaultProvider.h
@@ -5,6 +5,7 @@
 #include "../MyMod.h"
 #include "../PPGroup.h"
 #include <unordered_map>
+#include <optional>
 
 class DefaultProvider : public ProviderInterface
 {
@@ -22,7 +23,7 @@ public:
    YAML::Node getGroupsConfig() {return groups;}
    YAML::Node getGroupsData() {return groups;}
    tuple<string,vector<string>,YAML::Node,int> getPlayerData(Player* player);
-   unordered_map<string,tuple<string,vector<string>,YAML::Node,int>> getUsers();
+   optional<unordered_map<string,tuple<string,vector<string>,YAML::Node,int>>> getUsers();
    void setGroupData(PPGroup group,tuple<string,vector<string>,YAML::Node,int> data);
    void setGroupsData(unordered_map<string,tuple<string,vector<string>,YAML::Node,int>> data);
    void setPlayerData(Player* player,tuple<string,vector<string>,YAML::Node,int> data);
