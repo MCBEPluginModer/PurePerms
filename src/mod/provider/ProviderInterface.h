75468239672fecedb3acb8281e1b881f96c452b2
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-10 16:20:02 +0300

    Update ProviderInterface.h

--- src/mod/provider/ProviderInterface.h
+++ src/mod/provider/ProviderInterface.h
@@ -11,10 +11,10 @@ class ProviderInterface
 public:
     virtual ~ProviderInterface() = default;
 
-    virtual std::unordered_map<std::string, std::string> getGroupData(const PPGroup& group) = 0;
-    virtual std::vector<std::unordered_map<std::string, std::string>> getGroupsData() = 0;
-    virtual std::unordered_map<std::string, std::string> getPlayerData(const Player* player) = 0;
-    virtual std::vector<Player*> getUsers() = 0;
+    virtual YAML::Node getGroupData(const PPGroup& group) = 0;
+    virtual YAML::Node getGroupsData() = 0;
+    virtual tuple<string,vector<string>,YAML::Node,int> getPlayerData(const Player* player) = 0;
+    virtual unordered_map<string,tuple<string,vector<string>,YAML::Node,int>> getUsers() = 0;
     virtual void setGroupData(const PPGroup& group, const std::unordered_map<std::string, std::string>& tempGroupData) = 0;
     virtual void setGroupsData(const std::vector<std::unordered_map<std::string, std::string>>& tempGroupsData) = 0;
     virtual void setPlayerData(const Player* player, const std::unordered_map<std::string, std::string>& tempPlayerData) = 0;
