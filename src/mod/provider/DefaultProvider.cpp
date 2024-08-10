e395435e6a3021674d8e58c6ac1281c7ec44ae48
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-10 18:39:00 +0300

    Update DefaultProvider.cpp

--- src/mod/provider/DefaultProvider.cpp
+++ src/mod/provider/DefaultProvider.cpp
@@ -21,6 +21,77 @@ DefaultProvider::DefaultProvider(mcpm::PurePerms* _plugin)
   {
     in.close();
     in1.close();
+
+    // Добавляем группу "Guest"
+    YAML::Node guest;
+    guest["alias"] = "gst";
+    guest["isDefault"] = true;
+    guest["inheritance"] = YAML::Node(YAML::NodeType::Sequence);  // Пустая последовательность
+
+    YAML::Node guestPermissions;
+    guestPermissions.push_back("-essentials.kit");
+    guestPermissions.push_back("-essentials.kit.other");
+    guestPermissions.push_back("-pocketmine.command.me");
+    guestPermissions.push_back("pchat.colored.format");
+    guestPermissions.push_back("pchat.colored.nametag");
+    guestPermissions.push_back("pocketmine.command.list");
+    guestPermissions.push_back("pperms.command.ppinfo");
+    guest["permissions"] = guestPermissions;
     
+    guest["worlds"] = YAML::Node(YAML::NodeType::Sequence);  // Пустая последовательность
+
+    groups["Guest"] = guest;
+
+    // Добавляем группу "Admin"
+    YAML::Node admin;
+    admin["alias"] = "adm";
+    admin["inheritance"].push_back("Guest");
+
+    YAML::Node adminPermissions;
+    adminPermissions.push_back("essentials.gamemode");
+    adminPermissions.push_back("pocketmine.broadcast");
+    adminPermissions.push_back("pocketmine.command.gamemode");
+    adminPermissions.push_back("pocketmine.command.give");
+    adminPermissions.push_back("pocketmine.command.kick");
+    adminPermissions.push_back("pocketmine.command.teleport");
+    adminPermissions.push_back("pocketmine.command.time");
+    admin["permissions"] = adminPermissions;
+    
+    admin["worlds"] = YAML::Node(YAML::NodeType::Sequence);  // Пустая последовательность
+
+    groups["Admin"] = admin;
+
+    // Добавляем группу "Owner"
+    YAML::Node owner;
+    owner["alias"] = "owr";
+    owner["inheritance"].push_back("Admin");
+
+    YAML::Node ownerPermissions;
+    ownerPermissions.push_back("essentials");
+    ownerPermissions.push_back("pocketmine.command");
+    ownerPermissions.push_back("pperms.command");
+    owner["permissions"] = ownerPermissions;
+    
+    owner["worlds"] = YAML::Node(YAML::NodeType::Sequence);  // Пустая последовательность
+
+    groups["Owner"] = owner;
+
+    // Добавляем группу "OP"
+    YAML::Node op;
+    op["alias"] = "op";
+    op["inheritance"] = YAML::Node(YAML::NodeType::Sequence);  // Пустая последовательность
+
+    YAML::Node opPermissions;
+    opPermissions.push_back("*");
+    op["permissions"] = opPermissions;
+
+    op["worlds"] = YAML::Node(YAML::NodeType::Sequence);  // Пустая последовательность
+
+    groups["OP"] = op;
+
+    // Сохраняем в файл
+    std::ofstream fout("plugins/PurePerms/ranks.yaml");
+    fout << root;
+    fout.close();
   }
 }
