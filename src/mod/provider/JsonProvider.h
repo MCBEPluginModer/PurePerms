7a68f08f22d886acdc85ee4868c63f4f9adbbeda
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-11 08:27:33 +0300

    Create JsonProvider.h

---
+++ src/mod/provider/JsonProvider.h
@@ -0,0 +1,31 @@
+#include "ProviderInterface.h"
+#include <yaml-cpp/yaml.h>
+#include <ll/api/Logger.h>
+#include <mc/world/actor/player/Player.h>
+#include "../MyMod.h"
+#include "../PPGroup.h"
+#include <unordered_map>
+#include <optional>
+
+class JsonProvider : public ProviderInterface
+{
+   mcpm::PurePerms* plugin;
+   //YAML::Node groups,players;
+   inline std::string toLowerCase(const std::string& str) const 
+   {
+        std::string result = str;
+        std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+        return result;
+    }
+public:
+   JsonProvider(mcpm::PurePerms* _plugin);
+   YAML::Node getGroupData(PPGroup group);
+   //YAML::Node getGroupsConfig() {return groups;}
+   //YAML::Node getGroupsData() {return groups;}
+   tuple<string,vector<string>,YAML::Node,int> getPlayerData(Player* player);
+   optional<unordered_map<string,tuple<string,vector<string>,YAML::Node,int>>> getUsers();
+   void setGroupData(PPGroup& group,tuple<string,vector<string>,YAML::Node,int>& data);
+   void setGroupsData(unordered_map<string,tuple<string,vector<string>,YAML::Node,int>> data);
+   void setPlayerData(Player* player,tuple<string,vector<string>,YAML::Node,int> data);
+   void close() {}
+};
