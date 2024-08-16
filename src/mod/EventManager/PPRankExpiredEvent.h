682e3762ef26e48e81d99277a30f15d1ef0d358c
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-16 20:08:50 +0300

    Update PPRankExpiredEvent.h

--- src/mod/EventManager/PPRankExpiredEvent.h
+++ src/mod/EventManager/PPRankExpiredEvent.h
@@ -25,7 +25,7 @@ class PPRankExpiredEvent final : public ll::event::Cancellable<ll::event::Event>
     Level& mLevel;
     Player& mPlayer;
 public:
-    constexpr explicit PPRankExpiredEvent(Level& level,Player& player) : mLevel(level),mPlayer(player),ll::event::Cancellable(player) {}
+    constexpr explicit PPRankExpiredEvent(Level& level,Player& player) : mLevel(level),mPlayer(player),Cancellable(player) {}
 
     LLAPI void serialize(CompoundTag&) const override;
     LLAPI void deserialize(CompoundTag const&) override;
