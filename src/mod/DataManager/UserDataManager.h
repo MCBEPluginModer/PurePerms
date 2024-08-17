0cdaadacecffd005483d3711a6032f8cfa96e122
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-17 15:45:09 +0300

    Create UserDataManager.h

---
+++ src/mod/DataManager/UserDataManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "../MyMod.h"
+#include "../PPGroup.h"
+#include "EventManager/PPRankExpiredEvent.h"
+#include <optional>
+#include <vector>
+using namespace std;
+
+class UserDataManager
+{
+   mcpm::PurePerms* plugin;
+public:
+    YAML::Node getData(Player* player);
+    YAML::Node getExpDate(Player* player,string levelname = "");
+    optional<PPGroup> getGroup(Player* player,string levelname = "");
+    optional<YAML::Node> getNode(Player* player,string node);
+    vector<string> getUserPermissions(Player* player,string node);
+    optional<YAML::Node> getWorldData(Player* player,string levelname);
+    void removeNode(Player* player,string node);
+};
