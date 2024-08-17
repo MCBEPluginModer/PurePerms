dfbf0aca22b3e191ef665f7ce91b6968bcdea589
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-17 05:26:03 +0300

    Update PPRankExpiredEvent.cpp

--- src/mod/EventManager/PPRankExpiredEvent.cpp
+++ src/mod/EventManager/PPRankExpiredEvent.cpp
@@ -22,8 +22,8 @@ void PPRankExpiredEvent::serialize(CompoundTag& nbt) const {
 void PPRankExpiredEvent::deserialize(CompoundTag const& nbt) {
     ll::event::Cancellable<ll::event::Event>::deserialize(nbt); // Вызов метода десериализации базового класса
     // Преобразование типов указателей
-    level() = *reinterpret_cast<Level*>(nbt["world"].getInt64());
-    player() = *reinterpret_cast<Player*>(nbt["player"].getInt64());
+    level() = nbt["world"];
+    player() = nbt["player"];
 }
 
 
