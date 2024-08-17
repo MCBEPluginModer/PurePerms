db5d2ad7afd2b397ded4df8ebf5128cbdeb578a4
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-17 15:17:08 +0300

    Update PPRankExpiredEvent.h

--- src/mod/EventManager/PPRankExpiredEvent.h
+++ src/mod/EventManager/PPRankExpiredEvent.h
@@ -27,22 +27,21 @@ class PPRankExpiredEvent final : public ll::event::Cancellable<ll::event::Event>
 {
     Level& mLevel;
     Player& mPlayer;
-    PPGroup& mGroup;
 public:
-    constexpr explicit PPRankExpiredEvent(Level& level,Player& player,PPGroup& group) : mLevel(level),mPlayer(player),mGroup(group) Cancellable() {}
+    constexpr explicit PPRankExpiredEvent(Level& level,Player& player) : mLevel(level),mPlayer(player), Cancellable() {}
     LLNDAPI Level& level() const;
     LLNDAPI Player& player() const;
-    LLNDAPI PPGroup& group() const;
 };
 
 class PPRankChangedEvent  final : public ll::event::Cancellable<ll::event::Event>
 {
     Level& mLevel;
     Player& mPlayer;
+    PPGroup& mGroup;
 public:
-    constexpr explicit PPRankChangedEvent (Level& level,Player& player) : mLevel(level),mPlayer(player), Cancellable() {}
+    constexpr explicit PPRankChangedEvent (Level& level,Player& player,PPGroup& group) : mLevel(level),mPlayer(player),mGroup(group), Cancellable() {}
     LLNDAPI Level& level() const;
     LLNDAPI Player& player() const;
-
+    LLNDAPI PPGroup& group() const;
 };
 } // namespace ll::event::inline pp
