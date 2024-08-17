ee7c934806074c08c192a15c283f280725ac7f08
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-17 17:00:05 +0300

    Update UserDataManager.cpp

--- src/mod/DataManager/UserDataManager.cpp
+++ src/mod/DataManager/UserDataManager.cpp
@@ -1,11 +1,11 @@
 #include "UserDataManager.h"
 
-YAML::Node getData(Player* player)
+YAML::Node UserDataManager::getData(Player* player)
 {
   //return lugin->getProvider()->getPlayerData(player);
 }
 
-std::optional<int> getExpDate(Player* player,string levelname)
+std::optional<int> UserDataManager::getExpDate(Player* player,string levelname)
 {
    std::optional<YAML::Node> expDate;
 
@@ -24,3 +24,36 @@ std::optional<int> getExpDate(Player* player,string levelname)
 
    return expDate;
 }
+
+optional<PPGroup> UserDataManager::getGroup(Player* player,string levelname = "")
+{
+  std::optional<std::string> groupName;
+
+        if (!levelName.empty()) {
+            auto worldData = getWorldData(player, levelName);
+            if (worldData.has_value()) {
+                groupName = worldData.value()["group"].as<std::string>();
+            }
+        } else {
+            auto node = getNode(player, "group");
+            if (node.has_value()) {
+                groupName = node.value().as<std::string>();
+            }
+        }
+
+        if (!groupName.has_value()) {
+            return nullopt;
+        }
+
+       /* PPGroup* group = plugin->getGroup(groupName.value());
+        if (group == nullptr) {
+            //plugin->getLogger()->critical("Invalid group name found in " + player->getName() + "'s player data (World: " + (levelName.empty() ? "GLOBAL" : levelName) + ")");
+            //plugin->getLogger()->critical("Restoring the group data to 'default'");
+           // PPGroup* defaultGroup = plugin->getDefaultGroup(levelName);
+            //setGroup(player, defaultGroup, levelName);
+            //return defaultGroup;
+        }
+
+        return group;*/
+}
+
