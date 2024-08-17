37b7201ef5580e47dc53a6f0297946b8dc32f61e
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-08-17 15:16:09 +0300

    Update PPRankExpiredEvent.h

--- src/mod/EventManager/PPRankExpiredEvent.h
+++ src/mod/EventManager/PPRankExpiredEvent.h
@@ -5,6 +5,7 @@
 #include <mc/world/actor/player/Player.h>
 #include <mc/world/level/Level.h>
 #include <ll/api/event/Cancellable.h>
+#include "PPGroup.h"
 #include <iostream>
 
 /*#ifdef _WIN32
@@ -26,8 +27,20 @@ class PPRankExpiredEvent final : public ll::event::Cancellable<ll::event::Event>
 {
     Level& mLevel;
     Player& mPlayer;
+    PPGroup& mGroup;
 public:
-    constexpr explicit PPRankExpiredEvent(Level& level,Player& player) : mLevel(level),mPlayer(player), Cancellable() {}
+    constexpr explicit PPRankExpiredEvent(Level& level,Player& player,PPGroup& group) : mLevel(level),mPlayer(player),mGroup(group) Cancellable() {}
+    LLNDAPI Level& level() const;
+    LLNDAPI Player& player() const;
+    LLNDAPI PPGroup& group() const;
+};
+
+class PPRankChangedEvent  final : public ll::event::Cancellable<ll::event::Event>
+{
+    Level& mLevel;
+    Player& mPlayer;
+public:
+    constexpr explicit PPRankChangedEvent (Level& level,Player& player) : mLevel(level),mPlayer(player), Cancellable() {}
     LLNDAPI Level& level() const;
     LLNDAPI Player& player() const;
 
