c38b261da0a54a4cc910b00d83bba4324523c932
MCBEPluginModer <86790039+MCBEPluginModer@users.noreply.github.com>
2024-09-07 07:14:50 +0300

    Update JsonProvider.h

--- src/mod/provider/JsonProvider.h
+++ src/mod/provider/JsonProvider.h
@@ -40,7 +40,7 @@ public:
    rapidjson::Document getPlayerConfig(Player* player, bool onUpdate = false);
    tuple<string,vector<string>,YAML::Node,int> getPlayerData(const Player* player);
    optional<unordered_map<string,tuple<string,vector<string>,YAML::Node,int>>> getUsers();
-   void setGroupData(PPGroup& group,tuple<string,vector<string>,vector<string>,YAML::Node,int>& data);
+   void setGroupData(PPGroup& group, tuple<string,bool,vector<string>,vector<string>,YAML::Node>& tempGroupData);
    void setGroupsData(unordered_map<string,PPGroup> data);
    void setPlayerData(Player* player,tuple<string,vector<string>,YAML::Node,int> data);
    void close() {}
